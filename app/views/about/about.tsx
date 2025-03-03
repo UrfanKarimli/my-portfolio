267e7c028daab6e38cdba4300705a03a2e687d7f
Urfan <urfankarim@gmail.com>
2025-03-04 00:04:50 +0400

    Biography

--- app/views/about/about.tsx
+++ app/views/about/about.tsx
@@ -2,16 +2,16 @@
 
 
 import React from 'react'
-// import Biography from './components/Biography';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Education from './components/Education';
+import Biography from './components/biography';
 
 export default function AboutPage() {
   return (
     <div className='z-20 sticky bottom-0'>
       <div className=' mx-5 lg:mx-9 mt-20 h-full flex flex-col gap-16 '>
-        {/* <Biography/> */}
+        <Biography/>
         <Skills/>
         <Experience/>
         <Education/>
